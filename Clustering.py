acd5ca8b1f1dea0580f2f904275245e76b3fd64e
Mohd Ayan <ayanmh88@gmail.com>
2026-03-11 20:06:49 +0530

    Update Clustering.py

--- Clustering.py
+++ Clustering.py
@@ -1,1 +1,34 @@
+# Import libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
+# Sample dataset
+data = {
+    'Age': [22, 25, 47, 52, 46, 56, 23, 27, 48, 50],
+    'Income': [20000, 22000, 50000, 52000, 48000, 55000, 21000, 23000, 51000, 53000]
+}
+
+# Create DataFrame
+df = pd.DataFrame(data)
+
+# Select features
+X = df[['Age', 'Income']]
+
+# Create KMeans model
+kmeans = KMeans(n_clusters=2)
+
+# Fit model
+kmeans.fit(X)
+
+# Predict clusters
+df['Cluster'] = kmeans.predict(X)
+
+print(df)
+
+# Plot clusters
+plt.scatter(df['Age'], df['Income'], c=df['Cluster'])
+plt.xlabel("Age")
+plt.ylabel("Income")
+plt.title("K-Means Clustering")
+plt.show()
